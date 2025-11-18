80d5710ecf284f3daff5cf07bdd118a6e56d7d44
massivebird <33991373+massivebird@users.noreply.github.com>
2025-11-18 18:21:15 -0500

    wip: sequence does not account for suit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Tile {
     Man(u32),
     Pin(u32),
     Sou(u32),
 }
 
-fn to_tile(val: u32, c: char) -> Result<Tile, String> {
-    assert!(val > 9, "I don't make tiles outside 0..=9. (val={val})");
-
-    match (val, c) {
-        (val, 's') => Ok(Tile::Sou(val)),
-        (val, 'm') => Ok(Tile::Man(val)),
-        (val, 'p') => Ok(Tile::Pin(val)),
-        (val, c) => panic!("Unknown tile! v={val}, c={c}"),
+impl Tile {
+    const fn value(self) -> u32 {
+        match self {
+            Self::Man(val) | Self::Pin(val) | Self::Sou(val) => val,
+        }
     }
 }
 
@@ -36,4 +33,50 @@ fn main() {
     }
 
     dbg!(&tiles);
+
+    is_winning(tiles);
+}
+
+fn to_tile(val: u32, c: char) -> Result<Tile, String> {
+    assert!(val <= 9, "I don't make tiles outside 0..=9. (val={val})");
+
+    match (val, c) {
+        (val, 's') => Ok(Tile::Sou(val)),
+        (val, 'm') => Ok(Tile::Man(val)),
+        (val, 'p') => Ok(Tile::Pin(val)),
+        (val, c) => panic!("Unknown tile! v={val}, c={c}"),
+    }
+}
+
+fn is_winning(tiles: Vec<Tile>) -> u32 {
+    let mut score = 0;
+
+    let mut iter = tiles.iter().peekable();
+
+    for (i, tile) in tiles.iter().enumerate() {
+        if sequence_at(&tiles, i) {
+            println!("Sequence! Starts at i={i}, tile={tile:?}");
+        }
+    }
+
+    score
+}
+
+fn sequence_at(tiles: &[Tile], i: usize) -> bool {
+    // Do we have enough tiles to evaluate?
+    if tiles.iter().skip(i).take(3).count() != 3 {
+        return false;
+    }
+
+    let tiles = [tiles[i], tiles[i + 1], tiles[i + 2]];
+
+    tiles[1].value() == tiles[0].value() + 1 && tiles[2].value() == tiles[1].value() + 1
+}
+
+fn triplet(tiles: [Tile; 3]) -> bool {
+    tiles[0] == tiles[1] && tiles[1] == tiles[2]
+}
+
+fn pair(tiles: [Tile; 2]) -> bool {
+    tiles[0] == tiles[1]
 }
