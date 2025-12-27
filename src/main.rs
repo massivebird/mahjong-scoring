bf82e5752211e685d0c60c38038489be4465e81a
massivebird <33991373+massivebird@users.noreply.github.com>
2025-12-27 07:43:52 -0500

    added: the new structs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,48 @@
-use std::mem::{Discriminant, discriminant};
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Suit {
+    Man,
+    Pin,
+    Sou,
+    Honor,
+}
 
-#[derive(Debug, Copy, Clone, PartialEq)]
-enum Tile {
-    Man(u32),
-    Pin(u32),
-    Sou(u32),
-    Wind(u32),
+impl From<char> for Suit {
+    fn from(value: char) -> Self {
+        match value {
+            'm' => Self::Man,
+            's' => Self::Sou,
+            'p' => Self::Pin,
+            'z' => Self::Honor,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+struct Tile {
+    value: u32,
+    suit: Suit,
 }
 
 impl Tile {
-    const fn value(self) -> u32 {
-        match self {
-            Self::Man(val) | Self::Pin(val) | Self::Sou(val) | Self::Wind(val) => val,
-        }
-    }
-
-    const fn suit(self) -> Discriminant<Self> {
-        discriminant(&self)
+    const fn new(value: u32, suit: Suit) -> Self {
+        Self { value, suit }
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+enum Mentsu {
+    Triplet(Tile),
+    Quad(Tile),
+    Sequence(Tile, Tile, Tile),
+    Pair(Tile, Tile),
+}
+
 fn main() {
-    let s = "234m444456s777s22m";
+    let s = "22234m";
 
     let mut suit_vals: Vec<u32> = Vec::new();
-    let mut hand: Vec<Tile> = Vec::new();
+    let mut hand_tiles: Vec<Tile> = Vec::new();
 
     for c in s.chars() {
         if c.is_ascii_digit() {
@@ -32,117 +50,108 @@ fn main() {
             continue;
         }
 
-        for v in &suit_vals {
-            hand.push(to_tile(*v, c).unwrap());
+        for val in &suit_vals {
+            hand_tiles.push(Tile::new(*val, Suit::from(c)));
         }
 
         suit_vals.clear();
     }
 
-    dbg!(&hand);
-
-    if is_winning(&hand) {
-        println!("Hand {s} is a winning hand");
-        if all_simples(&hand) {
-            println!("And it's all simples! (1 Han)");
-        }
-    } else {
-        println!("Hand {s} is not a winning hand");
-    }
+    dbg!(&hand_tiles);
 }
 
-fn to_tile(val: u32, c: char) -> Result<Tile, String> {
-    assert!(val <= 9, "I don't make tiles outside 0..=9. (val={val})");
+// fn to_tile(val: u32, c: char) -> Result<Suit, String> {
+//     assert!(val <= 9, "I don't make tiles outside 0..=9. (val={val})");
 
-    match (val, c) {
-        (val, 's') => Ok(Tile::Sou(val)),
-        (val, 'm') => Ok(Tile::Man(val)),
-        (val, 'p') => Ok(Tile::Pin(val)),
-        (val, 'z') => Ok(Tile::Wind(val)),
-        (val, c) => panic!("Unknown tile! v={val}, c={c}"),
-    }
-}
+//     match (val, c) {
+//         (val, 's') => Ok(Suit::Sou(val)),
+//         (val, 'm') => Ok(Suit::Man(val)),
+//         (val, 'p') => Ok(Suit::Pin(val)),
+//         (val, 'z') => Ok(Suit::Honor(val)),
+//         (val, c) => panic!("Unknown tile! v={val}, c={c}"),
+//     }
+// }
 
-fn is_winning(tiles: &[Tile]) -> bool {
-    let mut num_trip_seq = 0;
-    let mut num_pairs = 0;
+// fn is_winning(tiles: &[Suit]) -> bool {
+//     let mut num_trip_seq = 0;
+//     let mut num_pairs = 0;
 
-    let mut i = 0;
-    while let Some(tile) = tiles.get(i) {
-        if sequence_at(tiles, i) {
-            println!("Sequence! Starts at i={i}, tile={tile:?}");
-            num_trip_seq += 1;
-            i += 3;
-            continue;
-        } else if triplet_at(tiles, i) {
-            println!("Triplet! Starts at i={i}, tile={tile:?}");
-            num_trip_seq += 1;
-            i += 3;
-            continue;
-        } else if pair_at(tiles, i) {
-            println!("Pair! Starts at i={i}, tile={tile:?}");
-            num_pairs += 1;
-            i += 2;
-            continue;
-        }
+//     let mut i = 0;
+//     while let Some(tile) = tiles.get(i) {
+//         if sequence_at(tiles, i) {
+//             println!("Sequence! Starts at i={i}, tile={tile:?}");
+//             num_trip_seq += 1;
+//             i += 3;
+//             continue;
+//         } else if triplet_at(tiles, i) {
+//             println!("Triplet! Starts at i={i}, tile={tile:?}");
+//             num_trip_seq += 1;
+//             i += 3;
+//             continue;
+//         } else if pair_at(tiles, i) {
+//             println!("Pair! Starts at i={i}, tile={tile:?}");
+//             num_pairs += 1;
+//             i += 2;
+//             continue;
+//         }
 
-        i += 1;
-    }
+//         i += 1;
+//     }
 
-    num_trip_seq == 4 && num_pairs == 1
-}
+//     num_trip_seq == 4 && num_pairs == 1
+// }
 
-fn all_simples(hand: &[Tile]) -> bool {
-    hand.iter().all(|t| {
-        matches!(t.value(), 2..=8) && matches!(t, Tile::Man(_) | Tile::Sou(_) | Tile::Pin(_))
-    })
-}
+// fn all_simples(hand: &[Suit]) -> bool {
+//     hand.iter().all(|t| {
+//         matches!(t.value(), 2..=8) && matches!(t, Suit::Man(_) | Suit::Sou(_) | Suit::Pin(_))
+//     })
+// }
 
-fn sequence_at(tiles: &[Tile], i: usize) -> bool {
-    // Do we have enough tiles to evaluate?
-    if tiles.iter().skip(i).take(3).count() != 3 {
-        return false;
-    }
+// fn sequence_at(tiles: &[Suit], i: usize) -> bool {
+//     // Do we have enough tiles to evaluate?
+//     if tiles.iter().skip(i).take(3).count() != 3 {
+//         return false;
+//     }
 
-    let tiles = [tiles[i], tiles[i + 1], tiles[i + 2]];
+//     let tiles = [tiles[i], tiles[i + 1], tiles[i + 2]];
 
-    // Honor/winds cannot form sequences.
-    if !tiles
-        .iter()
-        .all(|t| matches!(t, Tile::Man(_) | Tile::Sou(_) | Tile::Pin(_)))
-    {
-        return false;
-    }
+//     // Honor/winds cannot form sequences.
+//     if !tiles
+//         .iter()
+//         .all(|t| matches!(t, Suit::Man(_) | Suit::Sou(_) | Suit::Pin(_)))
+//     {
+//         return false;
+//     }
 
-    if tiles[0].suit() != tiles[1].suit() || tiles[1].suit() != tiles[2].suit() {
-        return false;
-    }
+//     if tiles[0].suit() != tiles[1].suit() || tiles[1].suit() != tiles[2].suit() {
+//         return false;
+//     }
 
-    tiles[1].value() == tiles[0].value() + 1 && tiles[2].value() == tiles[1].value() + 1
-}
+//     tiles[1].value() == tiles[0].value() + 1 && tiles[2].value() == tiles[1].value() + 1
+// }
 
-fn triplet_at(tiles: &[Tile], i: usize) -> bool {
-    // Do we have enough tiles to evaluate?
-    if tiles.iter().skip(i).take(3).count() != 3 {
-        return false;
-    }
+// fn triplet_at(tiles: &[Suit], i: usize) -> bool {
+//     // Do we have enough tiles to evaluate?
+//     if tiles.iter().skip(i).take(3).count() != 3 {
+//         return false;
+//     }
 
-    let tiles = [tiles[i], tiles[i + 1], tiles[i + 2]];
+//     let tiles = [tiles[i], tiles[i + 1], tiles[i + 2]];
 
-    if tiles[0].suit() != tiles[1].suit() || tiles[1].suit() != tiles[2].suit() {
-        return false;
-    }
+//     if tiles[0].suit() != tiles[1].suit() || tiles[1].suit() != tiles[2].suit() {
+//         return false;
+//     }
 
-    tiles[0] == tiles[1] && tiles[1] == tiles[2]
-}
+//     tiles[0] == tiles[1] && tiles[1] == tiles[2]
+// }
 
-fn pair_at(tiles: &[Tile], i: usize) -> bool {
-    // Do we have enough tiles to evaluate?
-    if tiles.iter().skip(i).take(2).count() != 2 {
-        return false;
-    }
+// fn pair_at(tiles: &[Suit], i: usize) -> bool {
+//     // Do we have enough tiles to evaluate?
+//     if tiles.iter().skip(i).take(2).count() != 2 {
+//         return false;
+//     }
 
-    let tiles = [tiles[i], tiles[i + 1]];
+//     let tiles = [tiles[i], tiles[i + 1]];
 
-    tiles[0].value() == tiles[1].value() && tiles[0].suit() == tiles[1].suit()
-}
+//     tiles[0].value() == tiles[1].value() && tiles[0].suit() == tiles[1].suit()
+// }
