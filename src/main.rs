5ad394960c762f3154b361d83983990835d1b5f2
massivebird <33991373+massivebird@users.noreply.github.com>
2025-12-28 06:41:36 -0500

    refactor: new score module

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use self::{
-    mentsu::{Mentsu, kind::Kind},
+    mentsu::Mentsu,
+    score::fu,
     suit::Suit,
     tile::Tile,
-    win_wait::WinWait,
     yaku::{Yaku, regular_yaku},
 };
 
 mod i13s;
 mod mentsu;
+mod score;
 mod suit;
 mod tile;
 mod win_wait;
@@ -90,40 +91,4 @@ fn valid_yaku(hand: &[Mentsu]) -> Vec<Yaku> {
     }
 
     ans
-}
-
-fn fu(hand: &[Mentsu], win_method: WinMethod) -> u32 {
-    let menzenchin = !hand.iter().any(|m| m.open && m.win_wait.is_none());
-
-    let mut total = if win_method == WinMethod::Ron && menzenchin {
-        30
-    } else {
-        20
-    };
-
-    for m in hand {
-        if let Some(wait) = m.win_wait
-            && !matches!(wait, WinWait::Ryanmen | WinWait::Shanpon)
-        {
-            total += 2;
-        }
-
-        let mut pts = match m.kind {
-            Kind::Triplet(_) => 4,
-            Kind::Quad(_) => 16,
-            _ => continue,
-        };
-
-        if m.open {
-            pts /= 2;
-        }
-
-        if m.honor() || m.entirely_terminal() {
-            pts *= 2;
-        }
-
-        total += pts;
-    }
-
-    total + (10 - total % 10) // Round to nearest 10
 }
---
+++ src/score.rs
@@ -0,0 +1,42 @@
+use crate::{
+    WinMethod,
+    mentsu::{Mentsu, kind::Kind},
+    win_wait::WinWait,
+};
+
+/// Computes the hand's fu.
+pub fn fu(hand: &[Mentsu], win_method: WinMethod) -> u32 {
+    let menzenchin = !hand.iter().any(|m| m.open && m.win_wait.is_none());
+
+    let mut total = if win_method == WinMethod::Ron && menzenchin {
+        30
+    } else {
+        20
+    };
+
+    for m in hand {
+        if let Some(wait) = m.win_wait
+            && !matches!(wait, WinWait::Ryanmen | WinWait::Shanpon)
+        {
+            total += 2;
+        }
+
+        let mut pts = match m.kind {
+            Kind::Triplet(_) => 4,
+            Kind::Quad(_) => 16,
+            _ => continue,
+        };
+
+        if m.open {
+            pts /= 2;
+        }
+
+        if m.honor() || m.entirely_terminal() {
+            pts *= 2;
+        }
+
+        total += pts;
+    }
+
+    total + (10 - total % 10) // Round to nearest 10
+}
